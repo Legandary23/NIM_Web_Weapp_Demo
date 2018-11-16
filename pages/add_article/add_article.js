b35375d7bb75022b46a66e07befbb8e8cefe32d1
Bill Feng <fengy@gatech.edu>
2018-11-15 20:07:19 -0800

    working front-end communicating with back-end

--- pages/add_article/add_article.js
+++ pages/add_article/add_article.js
@@ -2,6 +2,7 @@ import IMController from '../../controller/im.js';
 import { connect } from '../../redux/index.js';
 let app = getApp();
 let store = app.store;
+let backendAddr = 'http://127.0.0.1:3000/article/addArticle';
 let pageConfig = {
   data: {
     array: ["Wearable Devices", "Dota 2", "AI"],
@@ -15,7 +16,32 @@ let pageConfig = {
   },
   onLoad: function() {},
   submitArticle: function() {
-    
+    if (this.data.article_url) {
+      let articleInfo = {
+        "group_id": this.data.index,
+        "metadata": {
+          "article_url": this.data.article_url,
+          "article_tags": this.data.article_tags
+        }
+      }
+      wx.request({
+        url: backendAddr,
+        header: {
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        data: articleInfo,
+        complete: function (res) {
+          if (res == null || res.data == null) {
+            console.error('Error with backend server');
+            return;
+          }
+        }
+      })
+    }
+    else {
+      alert('Article URL should not be empty');
+    }
   },
   articleUrlInput: function (e) {
     this.setData({article_url: e.detail.value});
